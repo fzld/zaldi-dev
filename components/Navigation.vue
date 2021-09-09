341966c8bc48276745420db6e0631959d5ae8144
zaldi <fzlddd@gmail.com>
2021-09-09 21:36:28 +0700

    setups

---
+++ components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header>
+        <nav class="container flex flex-col sm:flex-row flex-wrap items-center pt-10 sm:pt-20 bg-zld-navy px-28">
+            <div class="flex flex-1 mb-6 sm:mb-0"><img src="~/assets/imgs/logo-yellow.svg" alt=""></div>
+            <div class="flex flex-1">
+            <ul class="flex flex-1 justify-center sm:justify-end items-center gap-6 text-zld-white text-sm font-black">
+                <li class="cursor-pointer m-0 sm:m-4"><a href="./about.html">about</a></li>
+                <li class="cursor-pointer m-0 sm:m-4"><a href="./portfolio.html">portfolio</a></li>
+                <li class="cursor-pointer m-0 sm:m-4"><a href="./contact.html">contact</a></li>
+            </ul>
+            </div>
+        </nav>
+    </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Navigation />
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
